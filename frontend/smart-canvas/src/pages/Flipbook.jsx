b64ebd2c6478ba331825665198d43e91cc751b54
Harsh Hindalekar <harshhindalekar051@gmail.com>
2025-09-13 21:50:06 +0530

    frontend seprated

--- frontend/smart-canvas/src/pages/Flipbook.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-export default function Flipbook(){
-  return <div>
-    <h2>Flipbook (coming soon)</h2>
-    <p>Frame by frame animation workspace will appear here (M4).</p>
-  </div>
-}
